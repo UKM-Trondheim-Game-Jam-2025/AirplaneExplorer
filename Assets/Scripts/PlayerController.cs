6caacd18266642eaccff82f03894b754dfabc7d9
Hilmir Snær Guðmundsson <hilmirsg@gmail.com>
2025-03-04 15:02:12 +0100

    Crouching Done
    Player Movement is now finished, press C to crouch, press Spacebar to jump and press Left Shift to Sprint.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,17 +9,27 @@ using static UnityEditor.Searcher.SearcherWindow.Alignment;
 public class PlayerController : MonoBehaviour
 {
     [Header("Movement")]
-    public float movementSpeed = 15.0f;
+    private float movementSpeed;
+    public float walkSpeed;
+    public float sprintSpeed;
 
     public float groundDrag;
 
+    [Header("Jumping")]
     public float jumpForce;
     public float jumpCooldown;
     public float airMultiplier;
     public bool _canJump;
 
+    [Header("Crouching")]
+    public float crouchSpeed;
+    public float crouchYScale;
+    private float startYScale;
+
     [Header("Keybinds")]
     public KeyCode jumpKey = KeyCode.Space;
+    public KeyCode sprintKey = KeyCode.LeftShift;
+    public KeyCode crouchKey = KeyCode.C;
 
     [Header("Ground Check")]
     public float playerHeight;
@@ -34,7 +44,16 @@ public class PlayerController : MonoBehaviour
 
     private Rigidbody rb;
 
-    public TextMeshProUGUI textBoxField;
+    [HideInInspector] public TextMeshProUGUI textBoxField;
+
+    public MovementState state;
+    public enum MovementState 
+    {
+        walking,
+        sprinting,
+        crouching,
+        air
+    }
 
     private void Start()
     {
@@ -42,6 +61,8 @@ public class PlayerController : MonoBehaviour
         rb.freezeRotation = true;
 
         _canJump = true;
+
+        startYScale = transform.localScale.y;
     }
     void Update()
     {
@@ -50,13 +71,13 @@ public class PlayerController : MonoBehaviour
 
         MovementInput();
         BalanceSpeed();
+        StateHandling();
 
         // Balances the unnecessary linear dragging when the Player is in motion.
         if (_isGrounded)
             rb.linearDamping = groundDrag;
         else
             rb.linearDamping = 0; 
-        BalanceSpeed();
     }
     public void FixedUpdate()
     {
@@ -70,11 +91,53 @@ public class PlayerController : MonoBehaviour
         // Checks if the Player can jump
         if (Input.GetKey(jumpKey) && _canJump && _isGrounded)
         {
-            Jump();
-
             _canJump = false;
 
+            Jump();
+
             Invoke(nameof(ResetJump), jumpCooldown);
+        }
+
+        // Start Crouching
+        if (Input.GetKeyDown(crouchKey))
+        {
+            transform.localScale = new Vector3(transform.localScale.x, crouchYScale, transform.localScale.z);
+            rb.AddForce(Vector3.down * 5f, ForceMode.Impulse);
+        }
+
+        // Stop Crouching
+        if (Input.GetKeyUp(crouchKey))
+        {
+            transform.localScale = new Vector3(transform.localScale.x, startYScale, transform.localScale.z);
+        }
+    }
+
+    private void StateHandling()
+    {
+        // Movement State - Crouching
+        if (Input.GetKey(crouchKey))
+        {
+            state = MovementState.crouching;
+            movementSpeed = crouchSpeed;
+        }
+        // Movement State - Sprinting
+        if (_isGrounded && Input.GetKey(sprintKey))
+        {
+            state = MovementState.sprinting;
+            movementSpeed = sprintSpeed;
+        }
+
+        // Movement State - Walking
+        else if (_isGrounded)
+        {
+            state = MovementState.walking;
+            movementSpeed = walkSpeed;
+        }
+
+        // Movement State - Air
+        else
+        {
+            state = MovementState.air;
         }
     }
     private void PlayerMovement()
@@ -100,9 +163,9 @@ public class PlayerController : MonoBehaviour
         {
             Vector3 limitVelocity = flatVelovity.normalized * movementSpeed;
             rb.linearVelocity = new Vector3(limitVelocity.x, rb.linearVelocity.y, limitVelocity.z);
-
-            textBoxField.SetText("Speed: " + flatVelovity.magnitude);
         }
+
+        textBoxField.SetText("Speed: " + flatVelovity.magnitude);
     }
 
     private void Jump()
