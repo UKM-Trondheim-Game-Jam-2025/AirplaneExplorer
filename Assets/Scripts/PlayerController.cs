433555cd547d6e0549eb02b36a4d373ea20ec5d1
Hilmir Snær Guðmundsson <hilmirsg@gmail.com>
2025-03-04 14:11:45 +0100

    Jumping done
    Make sure to adjust the jump power, it's a bit high at the moment.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,21 +4,33 @@ using UnityEngine;
 using Unity.Cinemachine;
 using UnityEngine.InputSystem;
 using TMPro;
+using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField]
-    private float movementSpeed = 15.0f;
+    [Header("Movement")]
+    public float movementSpeed = 15.0f;
 
-    [SerializeField]
-    private float groundDrag;
+    public float groundDrag;
 
+    public float jumpForce;
+    public float jumpCooldown;
+    public float airMultiplier;
+    public bool _canJump;
+
+    [Header("Keybinds")]
+    public KeyCode jumpKey = KeyCode.Space;
+
+    [Header("Ground Check")]
     public float playerHeight;
     public LayerMask groundLayerMask;
     public bool _isGrounded;
 
     [SerializeField]
     private Transform orientation;
+
+    private float horizontalInput;
+    private float verticalInput;
 
     private Rigidbody rb;
 
@@ -28,29 +40,59 @@ public class PlayerController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
+
+        _canJump = true;
     }
     void Update()
     {
+        // Check the ground to see if the Player is colliding with it.
         _isGrounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, groundLayerMask);
 
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        Vector3 movement = orientation.right * horizontal + orientation.forward * vertical;
+        MovementInput();
+        BalanceSpeed();
 
-        rb.AddForce(movement.normalized * (movementSpeed * 10f), ForceMode.Force);
-
+        // Balances the unnecessary linear dragging when the Player is in motion.
         if (_isGrounded)
-        {
             rb.linearDamping = groundDrag;
-        }
         else
-        {
             rb.linearDamping = 0; 
-        }
         BalanceSpeed();
     }
+    public void FixedUpdate()
+    {
+        PlayerMovement();
+    }
+    private void MovementInput()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
 
-    public void BalanceSpeed()
+        // Checks if the Player can jump
+        if (Input.GetKey(jumpKey) && _canJump && _isGrounded)
+        {
+            Jump();
+
+            _canJump = false;
+
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
+    }
+    private void PlayerMovement()
+    {
+        // Calculates the Movement Direction
+        Vector3 movement = orientation.right * horizontalInput + orientation.forward * verticalInput;
+        
+        // Will allow the Player to move if they're on the ground.
+        if(_isGrounded)
+        rb.AddForce(movement.normalized * movementSpeed * 10f, ForceMode.Force);
+
+        // Will not allow the Player to move if they're in the air.
+        else if (!_isGrounded)
+        rb.AddForce(movement.normalized * movementSpeed * 10f * airMultiplier, ForceMode.Force);
+
+    }
+
+    private void BalanceSpeed()
     {
         Vector3 flatVelovity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
 
@@ -61,5 +103,19 @@ public class PlayerController : MonoBehaviour
 
             textBoxField.SetText("Speed: " + flatVelovity.magnitude);
         }
+    }
+
+    private void Jump()
+    {
+        rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+        print("Player has jumped");
+
+    }
+
+    private void ResetJump()
+    {
+        _canJump = true;
     }
 }
