3474d9892a07ad5375ee295c8a7d174df329f6a4
masterITS <ingesela@gmail.com>
2025-03-05 16:47:44 +0100

    Portals work now!!!!

--- Assets/PortalSetUp/S_Teleportation.cs
+++ Assets/PortalSetUp/S_Teleportation.cs
@@ -1,15 +1,14 @@
 using System;
+using Unity.Cinemachine;
 using UnityEngine;
 
 public class S_Teleportation : MonoBehaviour
 {
     [SerializeField] private Transform player;
-    [SerializeField] private Transform receiver;
+    [SerializeField] private Transform connectedPortalCamera;
     
     private Rigidbody _playerRb;
     private PlayerController _playerController;
-    
-    private bool _isOverlapping = false;
     
     private void Start()
     {
@@ -25,36 +24,15 @@ public class S_Teleportation : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            _isOverlapping = true;
-        }
-    }
-    
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            _isOverlapping = false;
-        }
-    }
-
-    private void LateUpdate()
-    {
-        if (_isOverlapping)
-        {
+            _playerRb.linearVelocity = Vector3.zero;
             Vector3 portalToPlayer = player.position - transform.position;
-            float dotProduct = Vector3.Dot(transform.up, portalToPlayer);
-
-            if (dotProduct < 0f)
-            {
-                float rotationDifference = -Quaternion.Angle(transform.rotation, receiver.rotation);
-                rotationDifference += 180;
-                player.Rotate(Vector3.up, rotationDifference);
-
-                Vector3 positionOffset = Quaternion.Euler(0f, rotationDifference, 0f) * portalToPlayer;
-                player.position = receiver.position + positionOffset;
-                
-                _isOverlapping = false;
-            }
+            float rotationDifference = -Quaternion.Angle(transform.rotation, connectedPortalCamera.rotation);
+            rotationDifference += 180;
+            player.GetChild(2).GetComponent<CinemachinePanTilt>().PanAxis.Value = connectedPortalCamera.rotation.eulerAngles.y;
+            
+            Vector3 positionOffset = Quaternion.Euler(0f, rotationDifference, 0f) * portalToPlayer;
+            
+            player.position = connectedPortalCamera.position + positionOffset;
         }
     }
 }
